012756b9950df5cd684006cba2e620698e288712
Sami Khan <marketing.samikhan@gmail.com>
2026-02-11 06:35:15 -0800

    Connect frontend to serverless backend and prepare for Vercel deploy

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL;
-
 const api = axios.create({
-  baseURL: `${API_BASE}/api/blogs`, 
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
